be601a88e956eba9d4aae382fe99219ab2f366e4
kingkingakghak-commits <kingkingakghak@gmail.com>
2026-08-20 18:04:49 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.tgm.raidwatcher"
     compileSdk = 36
+
     defaultConfig {
         applicationId = "com.tgm.raidwatcher"
         minSdk = 26
@@ -12,7 +14,17 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
